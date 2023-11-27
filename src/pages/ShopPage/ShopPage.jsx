2eeeccf0b69737d17046b89661fad3740f8b3af9
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2023-11-28 00:45:39 +0700

    update/shop-page

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,57 +1,140 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+import {
+    MessageOutlined,
+    AppstoreOutlined,
+    UserOutlined,
+    ClockCircleOutlined,
+    PhoneOutlined,
+    MailOutlined,
+    PlusOutlined,
+} from '@ant-design/icons';
 
-import { AppstoreOutlined } from '@ant-design/icons';
-
-import styles from './Shop.module.scss';
-import { useState } from 'react';
-import { getItem } from '../../utils';
-import { Menu } from 'antd';
-import TableProduct from '../../components/TableProduct/TableProduct';
+import styles from './ShopPage.module.scss';
+import Button from '../../components/Button/Button';
+import CardProduct from '../../components/CardProduct/CardProduct';
 import * as userService from '../../services/userServices';
-import { useQuery } from 'react-query';
 
 const cx = classNames.bind(styles);
 
-function ShopPage() {
-    const [keySelected, setKeySelected] = useState('product');
-    const items = [getItem('Product', 'product', <AppstoreOutlined />)];
-    const handleOnClick = ({ key }) => {
-        setKeySelected(key);
-    };
-    const renderPage = (key) => {
-        switch (key) {
-            case 'product':
-                return <TableProduct isLoading={isLoadingProduct} data={dataProduct} refetch={refetch} />;
-            default:
-                return <></>;
-        }
-    };
+export default function ShopPage() {
+    const { id } = useParams();
+    const [isLoading, setIsLoading] = useState(false);
 
-    const getAllProduct = async () => {
-        const res = await userService.getProduct();
-        return res;
-    };
-
-    const {
-        isLoading: isLoadingProduct,
-        data: dataProduct,
-        refetch,
-    } = useQuery(['product'], getAllProduct, {
-        enabled: keySelected === 'product',
-    });
+    useEffect(() => {
+        const dataShop = userService.getUser(id);
+    }, [id]);
 
     return (
         <div className={cx('wrapper')}>
-            <Menu
-                defaultSelectedKeys={keySelected}
-                mode="inline"
-                onClick={handleOnClick}
-                style={{ width: 256, height: '100%' }}
-                items={items}
-            />
-            <div className={cx('wrapper-content')}>{renderPage(keySelected)}</div>
+            <div className={cx('wrapper-header')}>
+                <div className={cx('box')}>
+                    <div
+                        className={cx('wrapper-shop')}
+                        style={{
+                            backgroundImage: `url(https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/anh-bia-dep-10.jpg)`,
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                    >
+                        <div className={cx('shop-avatar')}>
+                            <img src="https://d1hjkbq40fs2x4.cloudfront.net/2016-01-31/files/1045.jpg" alt="" />
+                            <p className={cx('shop-name')}>Shop's name</p>
+                        </div>
+                        <div className={cx('shop-interact')}>
+                            <Button addIcon={<PlusOutlined />} leftIcon small outline className={cx('button-interact')}>
+                                Follow
+                            </Button>
+                            <Button
+                                addIcon={<MessageOutlined />}
+                                leftIcon
+                                small
+                                outline
+                                className={cx('button-interact')}
+                            >
+                                Chat
+                            </Button>
+                        </div>
+                    </div>
+                    <div className={cx('content')}>
+                        <div className={cx('block')}>
+                            <AppstoreOutlined />
+                            <span>Products:</span>
+                            <span className={cx('block-content')}>300</span>
+                        </div>
+                        <div className={cx('block')}>
+                            <UserOutlined />
+                            <span>Following: </span>
+                            <span className={cx('block-content')}>10</span>
+                        </div>
+                        <div className={cx('block')}>
+                            <MessageOutlined />
+                            <span>Response Time: </span>
+                            <span className={cx('block-content')}>In a few hours</span>
+                        </div>
+                    </div>
+                    <div className={cx('content')}>
+                        <div className={cx('block')}>
+                            <ClockCircleOutlined />
+                            <span>Joined: </span>
+                            <span className={cx('block-content')}>300</span>
+                        </div>
+                        <div className={cx('block')}>
+                            <PhoneOutlined />
+                            <span>Phone: </span>
+                            <span className={cx('block-content')}>10</span>
+                        </div>
+                        <div className={cx('block')}>
+                            <MailOutlined />
+                            <span>Email: </span>
+                            <span className={cx('block-content')}>phanhuuviet@gmail.com</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('wrapper-tab')}>
+                    <div className={cx('tabs', 'active')}>Home</div>
+                    <div className={cx('tabs')}>About Shop</div>
+                    <div className={cx('tabs')}>All Product</div>
+                </div>
+            </div>
+            <div className={cx('wrapper-content')}>
+                <div className={cx('block-type')}>
+                    <h2>Am thanh phu kien</h2>
+                    <div className={cx('list-product')}>
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                    </div>
+                </div>
+                <div className={cx('block-type')}>
+                    <h2>Am thanh phu kien</h2>
+                    <div className={cx('list-product')}>
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                    </div>
+                </div>
+                <div className={cx('block-type')}>
+                    <h2>Am thanh phu kien</h2>
+                    <div className={cx('list-product')}>
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                        <CardProduct />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-export default ShopPage;
