688d8bcc9197895a5cd1b8420af9b35a9f1821f6
hosykhanh <hosykhanh1108@gmail.com>
2023-12-26 20:49:53 +0700

    update/statistical

--- src/components/Statistical/Statistical.jsx
+++ src/components/Statistical/Statistical.jsx
@@ -104,28 +104,51 @@ function Statistical() {
                     </Col>
                 </Row>
             </div>
+
+            <div className={cx('wrapper-simple')}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                        <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
             <div className={cx('wrapper-container')}>
-                <div className={cx('wrapper-simple')}>
+                <div className={cx('wrapper-pie-chart')}>
                     <ResponsiveContainer width="100%" height="100%">
-                        <BarChart
-                            width={500}
-                            height={300}
-                            data={data}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                data={data1}
+                                cx="50%"
+                                cy="50%"
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
                             <Tooltip />
                             <Legend />
-                            <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-                            <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-                        </BarChart>
+                        </PieChart>
                     </ResponsiveContainer>
                 </div>
                 <div className={cx('wrapper-pie-chart')}>
