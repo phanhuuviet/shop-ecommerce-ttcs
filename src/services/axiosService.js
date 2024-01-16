0c8d6273143a440bb9b5c0ea10d6b6746ac345fa
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-01-17 00:25:35 +0700

    bugfix/config-axios

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 import tokenValid from '../utils/checkExpireToken';
-import * as authService from './authService';
+// import * as authService from './authService';
 
 const axiosJWT = axios.create();
 
 axiosJWT.interceptors.request.use(async function (config) {
     let token = JSON.parse(localStorage.getItem('access_token'));
-    if (tokenValid(token)) {
+    console.log('>>token', token);
+    if (token && tokenValid(token)) {
         config.headers.Accept = 'application/json';
         config.headers.Authorization = `Bearer ${token}`;
-    } else {
-        const data = await authService.refreshToken();
-        config.headers.Authorization = data?.access_token;
     }
+    // else {
+    // const data = await authService.refreshToken();
+    // config.headers.Authorization = data?.access_token;
+    // }
     return config;
 });
 
